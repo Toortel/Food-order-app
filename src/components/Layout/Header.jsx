6e1d2a488bf80cbf750d065f29922bbf332ed012
Toortel <103182939+Toortel@users.noreply.github.com>
2023-03-07 21:34:44 +0100

    Initial commit with basic header

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={classes["header-main"]}>
+        <h1 className={classes["header-title"]}>JSX-Meals</h1>
+        <div className={classes["header-cart"]}>(cart component here)</div>
+    </header>
+  );
+};
+
+export default Header;
