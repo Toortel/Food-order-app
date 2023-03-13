6bea52386a256ede7b6bceedd830a2dc207310ab
Jakub Domagała <falonix01@gmail.com>
2023-03-13 15:27:28 +0100

    Finished course project

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,18 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../context/ cart-context";
 
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = () => {
+  const [btnHighlight, setBtnHighlight] = useState(false);
   const ctx = useContext(CartContext);
 
   const numberOfItems = ctx.items.reduce((curVal, item) => {
     return curVal + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnHighlight ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (ctx.items.length === 0) {
+      return;
+    }
+    setBtnHighlight(true);
+    const timer = setTimeout(() => {
+      setBtnHighlight(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [ctx.items]);
+
   return (
-    <button className={classes.button} onClick={ctx.onShow}>
+    <button className={btnClasses} onClick={ctx.onShow}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
